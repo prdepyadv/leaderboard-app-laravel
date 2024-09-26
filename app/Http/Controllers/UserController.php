1ece8f8cf19ff941226094a971d6ddc02ad810b7
Pradeep Yadav <pradeep.yadav@myrealdata.in>
2024-09-26 22:58:38 +0530

    scoreReport endpoint added, and api format changed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -13,22 +14,37 @@ class UserController extends Controller
         $order = $request->query('order') ?? 'desc';
 
         $users = User::orderBy($sortby, $order)->get();
-        return response()->json($users);
+        return response()->json([
+            'error' => false,
+            'message' => 'Users retrieved successfully',
+            'data' => $users
+        ]);
     }
 
     public function findOne($id)
     {
         $user = User::findOrFail($id);
-        return response()->json($user);
+        return response()->json([
+            'error' => false,
+            'message' => 'User retrieved successfully',
+            'data' => $user
+        ]);
     }
 
     public function add(Request $request)
     {
-        $validated = $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255|unique:users',
             'age' => 'required|integer|min:1|max:60',
             'address' => 'required|string|max:255',
         ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => true,
+                'message' => 'Validation error occurred.',
+                'data' => $validator->errors()
+            ], 422);
+        }
 
         $user = User::create([
             'name' => $validated['name'],
@@ -37,7 +53,11 @@ class UserController extends Controller
             'points' => 0,
         ]);
 
-        return response()->json($user, 201);
+        return response()->json([
+            'error' => false,
+            'message' => 'User added successfully',
+            'data' => $user
+        ], 201);
     }
 
     public function addPoints($id)
@@ -46,7 +66,11 @@ class UserController extends Controller
         $user->points += 1;
         $user->save();
 
-        return response()->json($user);
+        return response()->json([
+            'error' => false,
+            'message' => 'User points incremented successfully',
+            'data' => $user
+        ]);
     }
 
     public function subPoints($id)
@@ -57,7 +81,11 @@ class UserController extends Controller
         }
         $user->save();
 
-        return response()->json($user);
+        return response()->json([
+            'error' => false,
+            'message' => 'User points decremented successfully',
+            'data' => $user
+        ]);
     }
 
     public function delete($id)
@@ -65,12 +93,38 @@ class UserController extends Controller
         $user = User::findOrFail($id);
         $user->delete();
 
-        return response()->json(['message' => 'User deleted successfully']);
+        return response()->json([
+            'error' => false,
+            'message' => 'User deleted successfully',
+            'data' => null
+        ]);
     }
 
     public function resetScores()
     {
         User::query()->update(['points' => 0]);
-        return response()->json(['message' => 'All user scores have been reset to 0']);
+        return response()->json([
+            'error' => false,
+            'message' => 'All user scores have been reset to 0',
+            'data' => null
+        ]);
+    }
+
+    public function scoreReport()
+    {
+        $users = User::orderBy("points", "desc")->get();
+        $groupedUsersByPoints = $users->groupBy('points')->map(function ($group) {
+            return [
+                'names' => $group->pluck('name'),
+                'average_age' => round($group->avg('age'))
+            ];
+        });
+
+        $groupedUsersByPoints = $groupedUsersByPoints->sortKeysDesc();
+        return response()->json([
+            'error' => false,
+            'message' => 'Score report generated successfully',
+            'data' => $groupedUsersByPoints
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,8 @@ use App\Http\Controllers\UserController;
 Route::prefix('api')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
     Route::post('/users', [UserController::class, 'add']);
-    Route::post('users/reset-scores', [UserController::class, 'resetScores']);
+    Route::get('users/scores/report', [UserController::class, 'scoreReport']);
+    Route::post('users/scores/reset', [UserController::class, 'resetScores']);
     Route::put('/user/{id}/increment', [UserController::class, 'addPoints']);
     Route::put('/user/{id}/decrement', [UserController::class, 'subPoints']);
     Route::get('/user/{id}', [UserController::class, 'findOne']);
